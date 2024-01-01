92ccd1c7f9b7d4dcac6d84f37e9556653f465746
taimoorKVD <taimoor.kingdomvision@gmail.com>
2024-01-01 20:56:07 +0500

    express router set

---
+++ functions/api.js
@@ -0,0 +1,57 @@
+const express = require('express'),
+    serverless = require('serverless-http'),
+    app = express(),
+    router = express().Router(),
+    PORT = process.env.PORT || 4000,
+    date = new Date();
+
+router.get('/', (req, res) => {
+    res.status(200).send('<h1>Welcome to Node Js - Social App</h1>');
+});
+
+router.get('/api/v1/instagram', (req, res) => {
+    const instaSocial = {
+        username: "Mohammad Taimoor Hussain | Instagram",
+        followers: 900,
+        follows: 10,
+        date: date.toDateString()
+    };
+
+    res.status(200).json(instaSocial);
+});
+
+router.get('/api/v1/facebook', (req, res) => {
+    const instaSocial = {
+        username: "Mohammad Taimoor Hussain | Facebook",
+        followers: 12000,
+        follows: 25,
+        date: date.toDateString()
+    };
+
+    res.status(200).json(instaSocial);
+});
+
+router.get('/api/v1/linkedin', (req, res) => {
+    const instaSocial = {
+        username: "Mohammad Taimoor Hussain | Linkedin",
+        followers: 5020,
+        follows: 17,
+        date: date.toDateString()
+    };
+
+    res.status(200).json(instaSocial);
+});
+
+router.get('/api/v1/uid/:uid/token/:token', (req, res) => {
+    const token = req.params.token,
+        uid = req.params.uid;
+
+    res.status(200).json({
+        uid,
+        token,
+        date: date.toDateString()
+    });
+});
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
